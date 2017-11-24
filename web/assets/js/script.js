a473081ec6e6a0a4f48a1b27df62b9c954c466ca
Karol <karol.gawrysiuk@valueadd.pl>
2017-11-24 14:41:27 +0100

    Notes 1.1

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,27 +1,38 @@
-window.onload = function () {
-    $("#taskTag").click(function () {
-        $(".modal").show();
+$("#task-tag").click(function () {
+    $('<div class="task-box">' +
+        '<i class="glyphicon glyphicon-trash"></i></div>').appendTo(".container-notes");
+});
+$("#question-tag").click(function () {
+    $('<div class="question-box">' +
+        '<i class="glyphicon glyphicon-trash"></i></div>').appendTo(".container-notes");
+});
 
-    });
+var licznik = 1;
 
-    $(".close").click(function () {
-        $(".modal").hide();
-    });
+$("#question-bool-tag").click(function () {
+    $('<div class="question-bool-box">' +
+        '<input type="checkbox">'+
+        '<input type="text" id="form1" class="form-control">'+
+        '<input  name="radio" type="radio">Yes' +
+        '<input  name="radio" type="radio">No' +
+        '<i class="glyphicon glyphicon-trash"></i></div>').appendTo(".container-notes");
+    licznik++;
+
+});
+
+$(".glyphicon-trash").click(function () {
+    $(".container-notes").children().remove();
+    alert("Delete");
 
 
-    $("#questionTag").click(function () {
-        $(".modal").show();
+});
+//
+// $("input[type='checkbox']").change(function(){
+//     if($(this).is(":checked")){
+//         $(this).parent().addClass("redBackground");
+//     }else{
+//         $(this).parent().removeClass("redBackground");
+//     }
+// });
 
-    });
-
-    $(".close").click(function () {
-        $(".form-add-question").hide();
-    });
-    $( function() {
-        $( "#sortable1, #sortable2" ).sortable({
-            connectWith: ".connectedSortable"
-        }).disableSelection();
-    } );
-
-};
 
