056739ac5ec6af1ea77e9ecbe5b2145e1b0073c2
Karol <karol.gawrysiuk@valueadd.pl>
2017-11-28 16:36:52 +0100

    Notes 1.2

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,38 +1,78 @@
+var licznik = 1;
 $("#task-tag").click(function () {
-    $('<div class="task-box">' +
+    $('<div class="task-box" id="box' + licznik + '">' +
+        '<input type="checkbox">' +
+        '<input type="text" class="form-control">' +
         '<i class="glyphicon glyphicon-trash"></i></div>').appendTo(".container-notes");
+
+    $(":checkbox").on("change", function () {
+        var that = this;
+        $(this).parent().css("background-color", function () {
+            return that.checked ? "#a5d6a7" : "";
+        });
+
+    });
+    $(".glyphicon-trash").click(function () {
+        var id = $(this).parent().attr("id");
+        $("#" + id).remove();
+    });
+    licznik++;
 });
 $("#question-tag").click(function () {
-    $('<div class="question-box">' +
-        '<i class="glyphicon glyphicon-trash"></i></div>').appendTo(".container-notes");
+    $('<div class="question-box" id="box' + licznik + '">' +
+        '<input type="checkbox">' +
+        '<input type="text" class="form-control">' +
+        '<i class="glyphicon glyphicon-trash"></i>' +
+        '<div class="asd"></div>' +
+        '<div class="asd"></div>' +
+        '<div class="asd"></div>' +
+        '<div class="asd"></div>' +
+        '</div>').appendTo(".container-notes");
+    licznik++;
+    $(".glyphicon-trash").click(function () {
+        var id = $(this).parent().attr("id");
+        $("#" + id).remove();
+    });
+    $(":checkbox").on("change", function () {
+        var that = this;
+        $(this).parent().css("background-color", function () {
+            return that.checked ? "#a5d6a7" : "";
+        });
+
+    });
 });
 
-var licznik = 1;
 
 $("#question-bool-tag").click(function () {
-    $('<div class="question-bool-box">' +
-        '<input type="checkbox">'+
-        '<input type="text" id="form1" class="form-control">'+
+    $('<div class="question-bool-box accordion" id="box' + licznik + '">' +
+        '<input type="checkbox">' +
+        '<input type="text" class="form-control">' +
         '<input  name="radio" type="radio">Yes' +
         '<input  name="radio" type="radio">No' +
-        '<i class="glyphicon glyphicon-trash"></i></div>').appendTo(".container-notes");
+        '<i class="glyphicon glyphicon-trash" id="delete' + licznik + '"></i></div>').appendTo(".container-notes");
     licznik++;
 
+    $(".glyphicon-trash").click(function () {
+        var id = $(this).parent().attr("id");
+        $("#" + id).remove();
+    });
+    $(":checkbox").on("change", function () {
+        var that = this;
+        $(this).parent().css("background-color", function () {
+            return that.checked ? "#a5d6a7" : "";
+        });
+
+    });
+
 });
 
-$(".glyphicon-trash").click(function () {
-    $(".container-notes").children().remove();
-    alert("Delete");
 
 
-});
-//
-// $("input[type='checkbox']").change(function(){
-//     if($(this).is(":checked")){
-//         $(this).parent().addClass("redBackground");
-//     }else{
-//         $(this).parent().removeClass("redBackground");
-//     }
-// });
+
+
+
+
+
+
 
 
